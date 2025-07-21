26b6553cabd797f5d1676f47925b4e99102d79d4
Masa <masa.beydoun@hotmail.com>
2025-07-21 16:24:41 +0300

    tables added to admin

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models.user import User
+from .models.favorite import Favorite
+from .models.userConsultation import UserConsultation
+
+from .models.consultant import Consultant
+from .models.registrationRequests import RegisterationRequests
+
+from .models.consultation import Consultation
+
+from .models.domain import Domain
+from .models.subdomain import SubDomain
+
+
+from .models.resource import Resource
+
+admin.site.register(User)
+admin.site.register(Favorite)
+admin.site.register(UserConsultation)
+admin.site.register(Consultant)
+admin.site.register(RegisterationRequests)
+admin.site.register(Consultation)
+admin.site.register(Domain)
+admin.site.register(SubDomain)
+admin.site.register(Resource)
+
