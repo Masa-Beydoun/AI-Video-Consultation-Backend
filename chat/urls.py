0b13447eba3245d8b6d60694b8f238a7617deed1
unknown <deedepsi@gmail.com>
2025-09-01 23:50:34 +0300

    General and voice messages have been added.

--- chat/urls.py
+++ chat/urls.py
@@ -3,10 +3,14 @@ from .views import MessageCreateView
 from .views import ChatListView
 from .views import ConsultantChatMessagesView
 from .views import ChatDeleteView
+from .views import QuestionConsultantsView
+from .views import VoiceToTextView
 
 urlpatterns = [
     path('ask/', MessageCreateView.as_view(), name='ask-question'),
     path('get_user_chats/', ChatListView.as_view(), name='chat-list'),
     path('messages/', ConsultantChatMessagesView.as_view(), name='chat-messages'),
     path('delete/', ChatDeleteView.as_view(), name='chat-delete'),
+    path('question_consultants/', QuestionConsultantsView.as_view(), name='consultants-by-question'),
+    path("voice_to_text/", VoiceToTextView.as_view(), name="voice-to-text"),
 ]
