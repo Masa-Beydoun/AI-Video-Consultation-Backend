8b28b68671c2f29c70c595d42324b3f28e734133
Masa <masa.beydoun@hotmail.com>
2025-09-07 07:36:09 +0300

    added

--- notifications/urls.py
+++ notifications/urls.py
@@ -2,12 +2,16 @@ from django.urls import path
 from .views import register_device_token
 from rest_framework.routers import DefaultRouter
 from .views import NotificationViewSet
+from django.urls import path, include
+
 
 router = DefaultRouter()
-router.register(r"notifications", NotificationViewSet, basename="notification")
+router.register(r"", NotificationViewSet, basename="notification")
 
 
 urlpatterns = [
     path("register/", register_device_token, name="register_device_token"),
+    path("", include(router.urls)),   # 👈 this line is missing
+
 
 ]
